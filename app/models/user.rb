d2bf792b2340d467b4ad0e9c4f08ba9d622f4921
masa555555 <vvv_aac@yahoo.co.jp>
2021-02-10 12:12:20 +0900

    バリテーションの修正
    # WHAT
    半角英数の混合しか登録できないバリテーション追加
    with optionを使いバリテーションをまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/
 
          validates :nickname, presence: true
          validates :email, presence:  true
-         validates :password, presence: true, length: { minimum: 6}
+         validates :password, presence: true, format: {with:VALID_PASSWORD_REGEX,
+        message: "doesn't input a small character and number with harf size"
+        }
          validates :password_confirmation, presence: true, length: { minimum: 6}
-         validates :family_name, presence:  true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-         validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-         validates :first_name_kana, presence:  true, format: {with: /\A[ァ-ヶー－]+\z/ }
+
+         with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+         validates :family_name, presence:  true
+         validates :first_name, presence: true
+         end
+         with_options format: {with: /\A[ァ-ヶー－]+\z/ } do
+         validates :family_name_kana, presence: true
+         validates :first_name_kana, presence:  true
+         end
          validates :birth_date, presence: true
          
 
